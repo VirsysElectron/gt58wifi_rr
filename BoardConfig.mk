600c4483635c1b2fdd421f02000d40d102cb641f
Nubianprince <evereth@hotmail.com>
2018-09-04 16:12:05 -0400

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,21 +12,25 @@ ALLOW_MISSING_DEPENDENCIES :=true
 USE_CUSTOM_AUDIO_POLICY :=0
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := ashyx_gt58wifi_defconfig
+
+
+
+TARGET_USERIMAGES_USE_EXT4 := true
 
 #TARGET_USES_QCOM_WCNSS_QMI := true
 
-#BOARD_KERNEL_BASE := 0x00000000
-#BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4
-#BOARD_KERNEL_IMAGE_NAME := zImage
-#BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
-#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
-#BOARD_CUSTOM_BOOTIMG := true
-#BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-#TARGET_KERNEL_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/gt58wifi
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+TARGET_KERNEL_CONFIG := ashyx_gt58wifi_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/ashyx
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
@@ -39,4 +43,4 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
 BOARD_PROVIDES_LIBRIL := false
 
 # inherit from the proprietary version
-#-include vendor/samsung/gt58wifi/BoardConfigVendor.mk
+-include vendor/samsung/gt58wifi/BoardConfigVendor.mk
