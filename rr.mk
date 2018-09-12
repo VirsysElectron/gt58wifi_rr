f854442dda511c27c1c521774ced8ce0c670c39c
Nubianprince <evereth@hotmail.com>
2018-09-12 15:48:56 -0400

    Update and rename cm.mk to rr.mk

--- rr.mk
+++ rr.mk
@@ -1,7 +1,7 @@
 # Inherit from common
 $(call inherit-product, device/samsung/gte-common/rr.mk)
 
-$(call inherit-product, device/samsung/gt58wifi/cm_gt58wifi.mk)
+$(call inherit-product, device/samsung/gt58wifi/rr_gt58wifi.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1024
@@ -9,7 +9,7 @@ TARGET_SCREEN_WIDTH := 768
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gt58wifi
-PRODUCT_NAME := cm_gt58wifi
+PRODUCT_NAME := rr_gt58wifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T350
 PRODUCT_MANUFACTURER := samsung
