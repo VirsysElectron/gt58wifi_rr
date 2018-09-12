f8d15d15d04423315521b7c5bd48298efec700ae
Nubianprince <evereth@hotmail.com>
2018-09-11 22:08:38 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,19 +6,23 @@ LOCAL_PATH := device/samsung/gt58wifi
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := gt58wifi,gt58wifixx,SM-T350
 
-ALLOW_MISSING_DEPENDENCIES :=true
+##################### ADDITIONS ######################################################
+
+#ALLOW_MISSING_DEPENDENCIES :=true
+
+
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
 
 # Audio
-USE_CUSTOM_AUDIO_POLICY :=0
+#USE_CUSTOM_AUDIO_POLICY :=0
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := ashyx_gt58wifi_defconfig
 
-
-
-TARGET_USERIMAGES_USE_EXT4 := true
-
-#TARGET_USES_QCOM_WCNSS_QMI := true
+#BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
+#BOARD_KERNEL_BASE := 0x80000000
+#BOARD_KERNEL_PAGESIZE := 2048
+#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 01e00000 --dt device/samsung/gt58wifi/dtb
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4
@@ -28,9 +32,33 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-TARGET_KERNEL_CONFIG := ashyx_gt58wifi_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/ashyx
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+#TARGET_KERNEL_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+#TARGET_PREBUILT_KERNEL := device/samsung/gt58wifi/kernel
+TARGET_SELINUX_DEFCONFIG := selinux_defconfig
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/samsung/gt58wifi
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+
+# Camera
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
+##################### ADDITIONS ######################################################
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
@@ -42,5 +70,4 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
 #RIL
 BOARD_PROVIDES_LIBRIL := false
 
-# inherit from the proprietary version
--include vendor/samsung/gt58wifi/BoardConfigVendor.mk
+
