d807240aa064ff1e4ee7ea0769edebed904058fa
Nubianprince <evereth@hotmail.com>
2018-09-15 10:28:10 -0400

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ TARGET_OTA_ASSERT_DEVICE := gt58wifi,gt58wifixx,SM-T350
 
 ##################### ADDITIONS ######################################################
 
-#ALLOW_MISSING_DEPENDENCIES :=true
+ALLOW_MISSING_DEPENDENCIES :=true
 
 
 # Use Snapdragon LLVM, if available
@@ -17,19 +17,18 @@ TARGET_USE_SDCLANG := true
 ANDROID_COMPILE_WITH_JACK := false
 
 # Audio
-#USE_CUSTOM_AUDIO_POLICY :=0
+USE_CUSTOM_AUDIO_POLICY :=0
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_SELINUX_DEFCONFIG := selinux_defconfig
 
 TARGET_BOARD_PLATFORM := msm8916
@@ -37,8 +36,17 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/samsung/gt58wifi
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
 #TARGET_KERNEL_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
 #TARGET_PREBUILT_KERNEL := device/samsung/gt58wifi/kernel
+
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_NO_BOOTLOADER := true
+
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
@@ -51,6 +59,7 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_USES_MEDIA_EXTENSIONS := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
 
 ##################### ADDITIONS ######################################################
 
--- rr_gt58wifi.mk
+++ rr_gt58wifi.mk
@@ -1,8 +1,12 @@
+#Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/gt58wifi/gt58wifi-vendor.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/device-common.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-
-$(call inherit-product, device/samsung/gt58wifi/device.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1024
@@ -17,3 +21,34 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+LOCAL_PATH := device/samsung/gt58wifi
+
+# System properties
+-include $(LOCAL_PATH)/system.prop
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/gt58wifi/overlay
+
+# Audio
+PRODUCT_COPY_FILES += \
+    device/samsung/gt58wifi/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    device/samsung/gt58wifi/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/samsung/gt58wifi/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    device/samsung/gt58wifi/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    device/samsung/gt58wifi/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/General_cal.acdb:system/etc/General_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/Global_cal.acdb:system/etc/Global_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/Handset_cal.acdb:system/etc/Handset_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/Headset_cal.acdb:system/etc/Headset_cal.acdb \
+    device/samsung/gt58wifi/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb
+
+# Ril
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.carrier=wifi-only \
+    ro.radio.noril=1
+    
+    PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=gt58wifi \
+    PRODUCT_NAME=gt58wifixx
