72bd404ed301c6bfa4abe29aaf2ea781251b21c3
Nubianprince <evereth@hotmail.com>
2018-09-20 23:02:23 -0400

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,122 @@
+# Inherit from common
+include device/samsung/gte-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/samsung/gt58wifi
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := gt58wifi,gt58wifixx,SM-T350
+
+##################### ADDITIONS ######################################################
+
+#ALLOW_MISSING_DEPENDENCIES :=true
+
+
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
+
+# Build system
+#USE_NINJA := false
+COMPILE_WITH_JACK := false
+
+# Audio
+#USE_CUSTOM_AUDIO_POLICY :=0
+# 
+
+
+TARGET_KERNEL_SOURCE := kernel/samsung/gt58wifi
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+
+# Architecture
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
+
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+
+
+# Init
+#TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+
+# Includes
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/msm8916-common/include
+
+# Kernel
+BOARD_DTBTOOL_ARG := -2
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+#TARGET_KERNEL_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+#TARGET_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfigg
+#TARGET_SELINUX_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+
+#TARGET_PREBUILT_KERNEL := device/samsung/gt58wifi/kernel
+
+
+
+#FORCE_32_BIT := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS:= true
+
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/msm8916-common/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/msm8916-common/recovery/recovery_keys.c
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+
+#TWRP
+TW_THEME := portrait_mdpi
+TW_BRIGHTNESS_PATH := "/sys/devices/soc.0/1a00000.qcom\x2cmdss_mdp/qcom\x2cmdss_fb_primary.139/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_NEW_ION_HEAP := true
+TW_TARGET_USES_QCOM_BSP := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_USB_STORAGE := true
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_INCLUDE_CRYPTO := true
+
+
+
+
+##################### ADDITIONS ######################################################
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Partition sizes
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
+#BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/msm8916-common/ril/
+
