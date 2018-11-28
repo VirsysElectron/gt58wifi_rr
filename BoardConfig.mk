48cca6993db886f3cf77eaad6d3157ea14201e60
Nubianprince <evereth@hotmail.com>
2018-11-28 15:58:14 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,150 +6,31 @@ LOCAL_PATH := device/samsung/gt58wifi
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := gt58wifi,gt58wifixx,SM-T350
 
+# Kernel
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Partition sizes
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
+
+#RIL
+BOARD_PROVIDES_LIBRIL := false
+
 ##################### ADDITIONS ######################################################
 
 #ALLOW_MISSING_DEPENDENCIES :=true
 
 
 # Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := true
+#TARGET_USE_SDCLANG := true
 
 
 # Build system
-USE_NINJA := false
-#COMPILE_WITH_JACK := false
-
-# Audio
-#USE_CUSTOM_AUDIO_POLICY :=0
-
-
-TARGET_KERNEL_SOURCE := kernel/samsung/gt58wifi
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
-
-# Architecture
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_CPU_CORTEX_A53 := true
-
-# Audio
-AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
-BOARD_USES_ALSA_AUDIO := true
-USE_CUSTOM_AUDIO_POLICY := 1
-USE_XML_AUDIO_POLICY_CONF := 1
-
-
-# Board CFLAGS
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-
-# Camera
-TARGET_PROVIDES_CAMERA_HAL := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-# Cpusets
-ENABLE_CPUSETS := true
-
-
-# Display
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
-
-# Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-
-# Includes
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/msm8916-common/include
-
-# Kernel
-BOARD_DTBTOOL_ARG := -2
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-#TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
-TARGET_KERNEL_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
-TARGET_VARIANT_CONFIG := msm8916_sec_gt58wifi_eur_defconfigg
-TARGET_SELINUX_CONFIG := msm8916_sec_gt58wifi_eur_defconfig
-
-#TARGET_PREBUILT_KERNEL := device/samsung/gt58wifi/kernel
-
-
-
-#FORCE_32_BIT := true
-
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS:= true
-
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-
-
-# Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/msm8916-common/rootdir/etc/fstab.qcom
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/msm8916-common/recovery/recovery_keys.c
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-
-#TWRP
-TW_THEME := portrait_mdpi
-TW_BRIGHTNESS_PATH := "/sys/devices/soc.0/1a00000.qcom\x2cmdss_mdp/qcom\x2cmdss_fb_primary.139/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_NEW_ION_HEAP := true
-TW_TARGET_USES_QCOM_BSP := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_NO_USB_STORAGE := true
-TW_MTP_DEVICE := /dev/mtp_usb
-TW_INCLUDE_CRYPTO := true
-
-
-
+#USE_NINJA := false
+ANDROID_COMPILE_WITH_JACK := false
 
 ##################### ADDITIONS ######################################################
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-
-# Partition sizes
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12138278912
-#BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# RIL
-BOARD_RIL_CLASS := ../../../device/samsung/msm8916-common/ril/
 
